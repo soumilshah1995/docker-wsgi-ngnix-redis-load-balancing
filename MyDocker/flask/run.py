5b278743bac43ef905af38a91db6c00c0c25f3d4
Soumil Shah <s.shah@jobtarget.com>
2020-05-09 09:01:24 -0400

    Added

--- MyDocker/flask/run.py
+++ MyDocker/flask/run.py
@@ -1,30 +1,41 @@
-from flask import Flask
-from flask_restful import Resource, Api
-import sys
-import os
-import json
-import redis
+
+try:
+    from flask import Flask
+    from flask_restful import Resource, Api
+    import sys
+    import os
+    import json
+    from flask import app,Flask, request
+    from flask_restful import Resource, Api, reqparse
+except Exception :
+    pass
 
 
 app = Flask(__name__)
 api = Api(app)
-port = 5000
-
-if sys.argv.__len__() > 1:
-    port = sys.argv[1]
 
 
 class HelloWorld(Resource):
+
+    def __init__(self):
+        self.name = parser.parse_args().get('name', None)
+        self.age = parser.parse_args().get('age', None)
+
     def get(self):
-        r = redis.Redis(host='redis', port=6379, db=0)
-        if r.ping():
-            message = "Redis Ok "
-        else:
-            message = "Failed ..."
-        return json.dumps({"Message":"ok : {}".format(message)})
 
+        DATABASENAME = os.getenv("DATABASENAME")
 
-api.add_resource(HelloWorld, '/fo')
+        return json.dumps({
+            "Database":DATABASENAME,
+            "name":self.name,
+            "age":self.age
+
+        })
+
+api.add_resource(HelloWorld, "/fo")
+parser = reqparse.RequestParser()
+parser.add_argument("name", type=str, required=True, help= "Name is required ")
+parser.add_argument("age", type=str, help= "age is not mandatory  ")
 
 
 if __name__ == '__main__':
